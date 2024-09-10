3a910fd81d488fe7c3ec566c78fb521321e51ca6
Yago Catalano Andújar <catalanoyagocolegio15@gmail.com>
2024-09-11 00:07:27 +0200

    fix with api request

--- Portfolio/Portfolio/src/app/pages/home/home.component.ts
+++ Portfolio/Portfolio/src/app/pages/home/home.component.ts
@@ -1,30 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent implements OnInit {
-  repos: any[] = [];
-
-  private apiUrl = 'https://api.github.com/user/repos';
-  private token = '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';
-
-  constructor(private http: HttpClient) {}
-
-  ngOnInit(): void {
-    this.getRepos();
-  }
-
-  getRepos(): void {
-    this.http.get<any[]>(this.apiUrl, {
-      headers: {
-        'Authorization': `token ${this.token}`
-      }
-    }).subscribe(repos => {
-      this.repos = repos;
-    });
-  }
-}
+export class HomeComponent {}
