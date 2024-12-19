9df1bad45dccde3d27ea87117be33c67c69d90bc
Yago Catalano Andújar <catalanoyagocolegio15@gmail.com>
2024-12-19 16:23:11 +0100

    nav y modo oscuro, falta bandera y cmabio de idioma

--- Subtitler/js/darkmode.js
+++ Subtitler/js/darkmode.js
@@ -1,12 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const darkModeBtn = document.getElementById('dark-mode-btn');
+    const darkModeBtn = document.getElementById('darkmode-toggle');
   
     darkModeBtn.addEventListener('click', function () {
       document.body.classList.toggle('dark-mode');
-      if (document.body.classList.contains('dark-mode')) {
-        darkModeBtn.textContent = 'Modo Claro';
-      } else {
-        darkModeBtn.textContent = 'Modo Oscuro';
-      }
+
     });
   });
